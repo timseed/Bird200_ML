57ce894a52d616f293adf23d7b5a49ca54e3ab77
Tim Seed <tim@sy-edm.com>
2019-04-05 16:28:37 +0800

    33% accuracy

--- ML.py
+++ ML.py
@@ -113,16 +113,16 @@ class_mode="categorical",
 target_size=(SIZE_X,SIZE_Y))
 
 model = Sequential()
-model.add(Conv2D(32, (3, 3), padding='same',
+model.add(Conv2D(48, (3, 3), padding='same',
                  input_shape=(SIZE_X,SIZE_Y,3)))
 model.add(Activation('relu'))
 model.add(Conv2D(SIZE_X, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
-model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(Conv2D(96, (3, 3), padding='same'))
 model.add(Activation('relu'))
-model.add(Conv2D(64, (3, 3)))
+model.add(Conv2D(96, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.3))
